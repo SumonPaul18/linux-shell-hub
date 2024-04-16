5acc19b7fd9a1bfb29506b17d43406ed7a32e192
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-04-16 16:06:15 +0600

    Update InstallDockerOnUbuntu.sh

--- InstallDockerOnUbuntu.sh
+++ InstallDockerOnUbuntu.sh
@@ -19,7 +19,7 @@ sudo apt-get update
 
 #Install the Docker packages:
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 #Verify the Docker Version:
 
