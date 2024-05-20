8a8c401dee992672a892f388b6c501ef2bac81be
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-05-20 17:02:44 +0600

    Update assign-ip-on-almalinux-9.sh

--- assign-ip-on-almalinux-9.sh
+++ assign-ip-on-almalinux-9.sh
@@ -2,11 +2,15 @@
 bgreen='\033[1;32m'
 red='\033[0;31m'
 nc='\033[0m'
-echo -e "${bgreen} Settings up Static IP Address ${nc} "
+bold="\033[1m"
+blink="\033[5m"
+echo -e "${bgreen}${bold}${blink} Settings up Static IP Address ${nc} "
 
 # Set Hostname
-echo -e "${bgreen}Type Hostname : ${nc}"
-read hostname
+#echo -e "${bgreen}Type Hostname : ${nc}"
+#read hostname
+
+read -p "$(echo -e "${COLOR}${bold}${blink}Type System Hostname: ${RESET}")" hostname
 
 hostnamectl set-hostname $hostname
 
@@ -16,11 +20,14 @@ dnf install dnf-plugins-core -y
 dnf config-manager --set-enabled crb -y
 dnf install epel-release -y
 sudo dnf install https://www.rdoproject.org/repos/rdo-release.el9.rpm -y
-dnf install centos-release-openstack-antelope -y
+#dnf install centos-release-openstack-antelope -y
+dnf install centos-release-openstack-zed -y
+dnf install centos-release-openstack-bobcat -y
+dnf install centos-release-openstack-yoga -y
 
 dnf clean all 
 yum clean all
-dnf update -y
+#dnf update -y
 
 yum install network-scripts -y
 
@@ -33,13 +40,13 @@ systemctl restart network
 
 # Verifying IP & MAC Addresses
 ip a
-\n
-\n
+
+echo -e "${bgreen}${bold}${blink} Network Settings ${nc} "
 
 read -p "Type static IP Interface Name: " STATIC_INTERFACE
 read -p "Type MAC for static Interface: " MAC_Address
 read -p "Type static IP Address with CIDR: " IP_ADDRESS
-read -p "Type Gateway4: " GATEWAY
+#read -p "Type Gateway4: " GATEWAY
 read -p "Type DNS: " DNS
 
 cat <<EOF | sudo tee /etc/sysconfig/network-scripts/ifcfg-$STATIC_INTERFACE
@@ -49,7 +56,7 @@ BOOTPROTO=static
 ONBOOT=yes
 IPADDR=$IP_ADDRESS
 PREFIX=24
-GATEWAY=$GATEWAY
+#GATEWAY=$GATEWAY
 DNS1=$DNS
 #DNS2=8.8.4.4
 DEVICE=$STATIC_INTERFACE
